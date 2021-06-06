62c1da7f2e852d2e6a2ebeb05168d432d0af8536
bart holmberg <bart.holmberg@gmail.com>
2021-06-06 15:24:14 -0700

    update comments
    update comments

--- makeCommand.bat
+++ makeCommand.bat
@@ -1,3 +1,3 @@
-
-
+% build command 
+% pip install -e .
 python.exe -c "import sys, setuptools, tokenize; sys.argv[0] = '"'"'D:\\repo\\pyk4a\\setup.py'"'"';    __file__='"'"'D:\\repo\\pyk4a\\setup.py'"'"';f=getattr(tokenize, '"'"'open'"'"', open)(__file__);code=f.read().replace('"'"'\r\n'"'"', '"'"'\n'"'"');f.close();exec(compile(code, __file__, '"'"'exec'"'"'))" develop --no-deps
